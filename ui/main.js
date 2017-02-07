29d7d30f5f4f6aee72b4786c93527dc3812cf260
Abhinay Sunny Bitra <abhinay.tob@gmail.com>
2017-02-07 23:46:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,14 @@ button.onclick = function(){
     request.open('GET', 'http://abhinay-sunny.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name').value;
+document.getElementById('submit_btn').onclick = function(){
+    
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list = '<li>' + names[i] + '</li>';
+    }
+    document.getElementById('namelist').innerHTML = list;
+};
