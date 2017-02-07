2d75f1678948e4848b27b6f4a160fd8392bb212c
Abhinay Sunny Bitra <abhinay.tob@gmail.com>
2017-02-07 22:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/article',function(req, res){
   res.send('Article One requested');
 });
+var counter = 0;
+app.get('/counter',function(req, res){
+  counter = counter + 1;
+  res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
