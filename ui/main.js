37ab43c53f18e48a96cf08ecd274268efcb44872
Abhinay Sunny Bitra <abhinay.tob@gmail.com>
2017-02-07 23:50:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,9 @@ document.getElementById('submit_btn').onclick = function(){
     
     var names = ['name1','name2','name3','name4'];
     var list = '';
-    for(var i=0;i<names.length;i++){
+    for (var i=0;i<names.length;i++){
         list = '<li>' + names[i] + '</li>';
+        document.getElementById('namelist').innerHTML = list;
     }
     document.getElementById('namelist').innerHTML = list;
 };
